37abdef892a9a19c7cc27c2a04512a70226b9398
EnzoDero <enzo.dero.04@gmail.com>
2026-03-03 21:06:45 +0100

    Initialisation API.

---
+++ opcg-api/src/main/java/com/dero/opcg_api/OpcgApiApplication.java
@@ -0,0 +1,13 @@
+package com.dero.opcg_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpcgApiApplication.class, args);
+	}
+
+}
---
+++ opcg-api/src/test/java/com/dero/opcg_api/OpcgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dero.opcg_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
